9a25e9852d69dcd6e901c7cb460843d39796dc58
Marco De Freitas <marcoantoniodfp@gmail.com>
2023-07-22 15:57:24 -0400

    Modificacion final

--- src/aside.py
+++ src/aside.py
@@ -1,5 +1,4 @@
 import customtkinter as ctk
-from src.calcular import calcularProp
 from src.combinaciones import combPosibles
 from tkinter import messagebox
 
@@ -17,22 +16,10 @@ class asideFrame(ctk.CTkFrame):
     self.combinaciones = ctk.CTkButton(self, text="Generar\ncombinatoria", font=("Helvetica", 14, "bold"), command = self.generarCombinatoria)
     self.combinaciones.pack(pady=20, padx=30)
 
-    # Boton-Calcular proposiciones especificas
-    self.calcular = ctk.CTkButton(self, text="Calcular\nCombinatoria", font=("Helvetica", 14, "bold"), command = self.calcular_operacion)
-    self.calcular.pack(pady=10, padx=30)
-
   # Metodo para obtener todas las combinaciones posibles
   def generarCombinatoria(self):
     if len(self.props) >= 2:
       self.toplevel = combPosibles(master = self)
-      self.toplevel.grab_set()
-    else:
-      messagebox.showwarning("Advertencia", "¡Debe ingresar al menos 2 proposiciones!")
-
-  # Metodo para calcular cierta operacion con las proposiciones almacenadas
-  def calcular_operacion(self):
-    if len(self.props)>=2:
-      self.toplevel = calcularProp(master = self)
       self.toplevel.grab_set()
     else:
       messagebox.showwarning("Advertencia", "¡Debe ingresar al menos 2 proposiciones!")
